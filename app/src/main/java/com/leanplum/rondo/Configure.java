39ea904d59ffcd9342c4f5014b622be66570961d
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2018-10-21 04:20:18 +0530

    config file

---
+++ app/src/main/java/com/leanplum/rondo/Configure.java
@@ -0,0 +1,27 @@
+// Copyright 2017 Leanplum, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This is an auto generated file, expect changes to be overwritten.
+
+package com.leanplum.rondo;
+
+class Configure {
+  static final String APP_ID = "app_ve9UCNlqI8dy6Omzfu1rEh6hkWonNHVZJIWtLLt6aLs";
+  static final String PRODUCTION_KEY = "prod_D5ECYBLrRrrOYaFZvAFFHTg1JyZ2Llixe5s077Lw3rM";
+  static final String DEVELOPMENT_KEY = "dev_cKF5HMpLGqhbovlEGMKjgTuf8AHfr2Jar6rrnNhtzQ0";
+
+  static final String API_HOST_NAME = "www.leanplum.com";
+  static final Boolean API_SSL = true;
+
+  static final String SOCKET_HOST_NAME = "dev.leanplum.com";
+  static final Integer SOCKET_PORT = 80;
+}
