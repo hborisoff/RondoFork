88493c99d5342af75e979f6a2c577e950667bd80
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2018-12-02 08:33:45 -0800

    add titles

--- app/src/main/java/com/leanplum/rondo/AppCreateActivity.java
+++ app/src/main/java/com/leanplum/rondo/AppCreateActivity.java
@@ -35,6 +35,7 @@ public class AppCreateActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 LeanplumApp app = createApp();
+                finish();
             }
         });
     }
