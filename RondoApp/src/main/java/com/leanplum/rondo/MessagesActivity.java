4b3c6453e2e495d53692f90a48b4c9dfcd5c8f75
Hristo Borisov <hristo.borisov@leanplum.com>
2021-06-24 15:27:20 +0300

    Add buttons to log impressions and triggers for in-app.

--- RondoApp/src/main/java/com/leanplum/rondo/MessagesActivity.java
+++ RondoApp/src/main/java/com/leanplum/rondo/MessagesActivity.java
@@ -1,6 +1,9 @@
 package com.leanplum.rondo;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,9 +12,15 @@ import android.widget.ListView;
 
 import com.leanplum.Leanplum;
 
+import com.leanplum.internal.Constants;
+import com.leanplum.internal.Log;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class MessagesActivity extends AppCompatActivity {
+
+    private static final String LOG_IMPRESSIONS = "Log impression occurrences";
+    private static final String LOG_TRIGGERS = "Log trigger occurrences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +29,8 @@ public class MessagesActivity extends AppCompatActivity {
 
         final ListView listview = findViewById(R.id.listview);
         String[] values = new String[] { "alert", "centerPopup", "confirm",
-                "interstitial", "richInterstitial", "webInterstitial", "banner"};
+                "interstitial", "richInterstitial", "webInterstitial", "banner",
+                LOG_IMPRESSIONS, LOG_TRIGGERS};
 
         final ArrayList<String> list = new ArrayList<String>();
         for (int i = 0; i < values.length; ++i) {
@@ -36,9 +46,61 @@ public class MessagesActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, final View view,
                                     int position, long id) {
                 final String item = (String) parent.getItemAtPosition(position);
-                Leanplum.track(item);
+                if (LOG_IMPRESSIONS.equals(item)) {
+                    logImpressionOccurrences();
+                } else if (LOG_TRIGGERS.equals(item)) {
+                    logTriggerOccurrences();
+                } else {
+                    Leanplum.track(item);
+                }
             }
 
         });
+    }
+
+    private void logImpressionOccurrences() {
+        String prefix = String.format(Constants.Defaults.MESSAGE_IMPRESSION_OCCURRENCES_KEY, "");
+
+        Context context = Leanplum.getContext();
+        SharedPreferences prefs =
+            context.getSharedPreferences("__leanplum_messaging__", Context.MODE_PRIVATE);
+        Map<String, ?> all = prefs.getAll();
+
+        if (all.isEmpty()) {
+            Log.d("No impression occurrences yet.");
+            return;
+        }
+
+        for (Map.Entry<String, ?> entry : all.entrySet()) {
+            if (entry.getKey().startsWith(prefix)) {
+                String json = (String) entry.getValue();
+                if (!TextUtils.isEmpty(json)) {
+                    Log.d("messageId=" + entry.getKey().replace(prefix, "") + " -> " + json);
+                }
+            }
+        }
+    }
+
+    private void logTriggerOccurrences() {
+        String prefix = String.format(Constants.Defaults.MESSAGE_TRIGGER_OCCURRENCES_KEY, "");
+
+        Context context = Leanplum.getContext();
+        SharedPreferences prefs =
+            context.getSharedPreferences("__leanplum_messaging__", Context.MODE_PRIVATE);
+        Map<String, ?> all = prefs.getAll();
+
+        if (all.isEmpty()) {
+            Log.d("No trigger occurrences yet.");
+            return;
+        }
+
+        for (Map.Entry<String, ?> entry : all.entrySet()) {
+            if (entry.getKey().startsWith(prefix)) {
+                Object value = entry.getValue();
+                if (value != null) {
+                    Log.d("messageId=" + entry.getKey().replace(prefix, "") + " -> " + value);
+                }
+            }
+        }
     }
 }
